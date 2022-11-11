861b02fb22572c375d68df07c4a19f0c32e5ad8b
Daniel Chen <danielschen61@gmail.com>
2022-11-11 14:28:31 -0800

    randomized datasets after loading

--- train_models/train_baseline_cnn.py
+++ train_models/train_baseline_cnn.py
@@ -7,74 +7,99 @@ from sklearn.preprocessing import LabelEncoder
 
 import numpy as np
 import glob
+import random
 
 
 # Preprocessing the image dataset -----
-train_data = []
-val_data = []
-test_data = []
+# train_data = []
+# val_data = []
+# test_data = []
 
-train_labels = []
-val_labels = []
-test_labels = []
+# train_labels = []
+# val_labels = []
+# test_labels = []
+
+train_set = []
+val_set = []
+test_set = []
 
 # Training dataset -----
 for image in glob.glob("image_data/chest_xray/train/NORMAL/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    train_data.append(image)
-    train_labels.append('normal')
+    train_set.append((image, 'normal'))
+    # train_data.append(image)
+    # train_labels.append('normal')
 
 for image in glob.glob("image_data/chest_xray/train/PNEUMONIA_bacteria/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    train_data.append(image)
-    train_labels.append('pneumonia_bacteria')
+    train_set.append((image, 'pneumonia_bacteria'))
+    # train_data.append(image)
+    # train_labels.append('pneumonia_bacteria')
 
 for image in glob.glob("image_data/chest_xray/train/PNEUMONIA_virus/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    train_data.append(image)
-    train_labels.append('pneumonia_virus')
+    train_set.append((image, 'pneumonia_virus'))
+    # train_data.append(image)
+    # train_labels.append('pneumonia_virus')
 
 # Validation dataset -----
 for image in glob.glob("image_data/chest_xray/val/NORMAL/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    val_data.append(image)
-    val_labels.append('normal')
+    val_set.append((image, 'normal'))
+    # val_data.append(image)
+    # val_labels.append('normal')
 
 for image in glob.glob("image_data/chest_xray/val/PNEUMONIA_bacteria/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    val_data.append(image)
-    val_labels.append('pneumonia_bacteria')
+    val_set.append((image, 'pneumonia_bacteria'))
+    # val_data.append(image)
+    # val_labels.append('pneumonia_bacteria')
 
 for image in glob.glob("image_data/chest_xray/val/PNEUMONIA_virus/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    val_data.append(image)
-    val_labels.append('pneumonia_virus')
+    val_set.append((image, 'pneumonia_virus'))
+    # val_data.append(image)
+    # val_labels.append('pneumonia_virus')
 
 # Test dataset -----
 for image in glob.glob("image_data/chest_xray/test/NORMAL/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    test_data.append(image)
-    test_labels.append('normal')
+    test_set.append((image, 'normal'))
+    # test_data.append(image)
+    # test_labels.append('normal')
 
 for image in glob.glob("image_data/chest_xray/test/PNEUMONIA_bacteria/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    test_data.append(image)
-    test_labels.append('pneumonia_bacteria')
+    test_set.append((image, 'pneumonia_bacteria'))
+    # test_data.append(image)
+    # test_labels.append('pneumonia_bacteria')
 
 for image in glob.glob("image_data/chest_xray/test/PNEUMONIA_virus/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
-    test_data.append(image)
-    test_labels.append('pneumonia_virus')
+    test_set.append((image, 'pneumonia_virus'))
+    # test_data.append(image)
+    # test_labels.append('pneumonia_virus')
 
+random.shuffle(train_set)
+random.shuffle(val_set)
+random.shuffle(test_set)
+
+train_data = list(zip(*train_set))[0]
+val_data = list(zip(*val_set))[0]
+test_data = list(zip(*test_set))[0]
+
+train_labels = list(zip(*train_set))[1]
+val_labels = list(zip(*val_set))[1]
+test_labels = list(zip(*test_set))[1]
 
 X_train = np.array(train_data).astype('float32') / 255
 X_val = np.array(val_data).astype('float32') / 255
