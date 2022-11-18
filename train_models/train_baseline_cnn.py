7a876bfe4d04edafb180e926ff6d3a94a72f1ebd
Daniel Chen <danielschen61@gmail.com>
2022-11-17 20:34:55 -0800

    Changed to train model using original dataset

--- train_models/train_baseline_cnn.py
+++ train_models/train_baseline_cnn.py
@@ -1,3 +1,4 @@
+from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import layers, Model, optimizers
 from tensorflow.keras.applications.vgg16 import VGG16
@@ -9,39 +10,59 @@ import tensorflow_io as tfio
 import numpy as np
 import glob
 import random
-import os
 
 
 # Preprocessing the image dataset -----
 train_set = []
 test_set = []
 
-for file in glob.glob('image_data/stage_2_train_normal_full/*.*'):
-    image_bytes = tf.io.read_file(file)
-    image = tfio.image.decode_dicom_image(image_bytes, dtype=tf.uint16)
-    resized_image = tf.squeeze(tf.image.resize(image, size=(224, 224)), [0])
-    resized_image = tf.image.grayscale_to_rgb(resized_image)
-    resized_image = np.array(resized_image)
-    train_set.append((resized_image, 'normal'))
+# Training dataset -----
+for image in glob.glob("image_data/chest_xray/train/NORMAL/*.*"):
+    image = load_img(image, color_mode='rgb', target_size=(224, 224))
+    image = np.array(image)
+    train_set.append((image, 'normal'))
 
-for file in glob.glob('image_data/stage_2_train_pneumonia_full/*.*'):
-    image_bytes = tf.io.read_file(file)
-    image = tfio.image.decode_dicom_image(image_bytes, dtype=tf.uint16)
-    resized_image = tf.squeeze(tf.image.resize(image, size=(224, 224)), [0])
-    resized_image = tf.image.grayscale_to_rgb(resized_image)
-    resized_image = np.array(resized_image)
-    train_set.append((resized_image, 'pneumonia'))
+
+for image in glob.glob("image_data/chest_xray/train/PNEUMONIA_bacteria/*.*"):
+    image = load_img(image, color_mode='rgb', target_size=(224, 224))
+    image = np.array(image)
+    train_set.append((image, 'pneumonia_bacteria'))
+
+for image in glob.glob("image_data/chest_xray/train/PNEUMONIA_virus/*.*"):
+    image = load_img(image, color_mode='rgb', target_size=(224, 224))
+    image = np.array(image)
+    train_set.append((image, 'pneumonia_virus'))
+
+# Test dataset -----
+for image in glob.glob("image_data/chest_xray/test/NORMAL/*.*"):
+    image = load_img(image, color_mode='rgb', target_size=(224, 224))
+    image = np.array(image)
+    test_set.append((image, 'normal'))
+
+for image in glob.glob("image_data/chest_xray/test/PNEUMONIA_bacteria/*.*"):
+    image = load_img(image, color_mode='rgb', target_size=(224, 224))
+    image = np.array(image)
+    test_set.append((image, 'pneumonia_bacteria'))
+
+for image in glob.glob("image_data/chest_xray/test/PNEUMONIA_virus/*.*"):
+    image = load_img(image, color_mode='rgb', target_size=(224, 224))
+    image = np.array(image)
+    test_set.append((image, 'pneumonia_virus'))
 
 random.shuffle(train_set)
+random.shuffle(test_set)
 
 train_data = list(zip(*train_set))[0]
 train_labels = list(zip(*train_set))[1]
+test_data = list(zip(*test_set))[0]
+test_labels = list(zip(*test_set))[1]
 
 X_train = np.array(train_data).astype('float32') / 255
+X_test = np.array(test_data).astype('float32') / 255
 
 lb = LabelEncoder()
 y_train = to_categorical(lb.fit_transform(train_labels))
-
+y_test = to_categorical(lb.fit_transform(test_labels))
 
 # Build the model -----
 vgg = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
@@ -56,16 +77,19 @@ x = layers.Dense(128, activation='relu')(x)
 x = layers.Dropout(0.4)(x)
 x = layers.Dense(64, activation='relu')(x)
 x = layers.Dropout(0.2)(x)
-x = layers.Dense(2, activation='softmax')(x)
+x = layers.Dense(3, activation='softmax')(x)
 model = Model(inputs=vgg.input, outputs=x)
 
 # More hyperparameters
 learning_rate = 7.5e-7
 batch_size = 32
-epochs = 40
+epochs = 30
 optimizer = optimizers.Adam(learning_rate)
+validation_split = 0.2
 
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=[Precision(), Recall()])
-history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
 print("Training done...\n")
 
+print("Results on Test Dataset")
+results = model.evaluate(X_test, y_test, batch_size=batch_size)
