14476d88e314b36cbab0e4c5ce4195fbab45e568
Daniel Chen <danielschen61@gmail.com>
2022-11-20 18:22:43 -0800

    Changed paths

--- train_models/train_resnet152V2_model.py
+++ train_models/train_resnet152V2_model.py
@@ -15,34 +15,34 @@ train_set = []
 test_set = []
 
 # Training dataset -----
-for image in glob.glob("image_data/chest_xray/train/NORMAL/*.*"):
+for image in glob.glob("../image_data/chest_xray/train/NORMAL/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
     train_set.append((image, 'normal'))
 
 
-for image in glob.glob("image_data/chest_xray/train/PNEUMONIA_bacteria/*.*"):
+for image in glob.glob("../image_data/chest_xray/train/PNEUMONIA_bacteria/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
     train_set.append((image, 'pneumonia_bacteria'))
 
-for image in glob.glob("image_data/chest_xray/train/PNEUMONIA_virus/*.*"):
+for image in glob.glob("../image_data/chest_xray/train/PNEUMONIA_virus/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
     train_set.append((image, 'pneumonia_virus'))
 
 # Test dataset -----
-for image in glob.glob("image_data/chest_xray/test/NORMAL/*.*"):
+for image in glob.glob("../image_data/chest_xray/test/NORMAL/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
     test_set.append((image, 'normal'))
 
-for image in glob.glob("image_data/chest_xray/test/PNEUMONIA_bacteria/*.*"):
+for image in glob.glob("../image_data/chest_xray/test/PNEUMONIA_bacteria/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
     test_set.append((image, 'pneumonia_bacteria'))
 
-for image in glob.glob("image_data/chest_xray/test/PNEUMONIA_virus/*.*"):
+for image in glob.glob("../image_data/chest_xray/test/PNEUMONIA_virus/*.*"):
     image = load_img(image, color_mode='rgb', target_size=(224, 224))
     image = np.array(image)
     test_set.append((image, 'pneumonia_virus'))
@@ -81,7 +81,7 @@ model = Model(inputs=resnet.input, outputs=x)
 # More hyperparameters
 learning_rate = 7.5e-7
 batch_size = 32
-epochs = 30
+epochs = 35
 optimizer = optimizers.Adam(learning_rate)
 validation_split = 0.2
 
@@ -91,15 +91,4 @@ print("Training done...\n")
 
 print("Results on Test Dataset")
 results = model.evaluate(X_test, y_test, batch_size=batch_size)
-
-
-
-
-
-
-
-
-
-
-
 
